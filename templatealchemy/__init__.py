811a6472644f875744fc030b69629d1e2b832db5
grabner <pjg.github@ubergrabner.net>
2013-07-06 13:27:11 -0400

    moved to TA-standard __init__.py

--- templatealchemy/__init__.py
+++ templatealchemy/__init__.py
@@ -12,7 +12,10 @@ try:
 except ImportError:
   __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 
-from .engine import *
+try:
+  from templatealchemy.engine import *
+except ImportError:
+  pass
 
 #------------------------------------------------------------------------------
 # end of $Id$
