b3f8cd41b83070d4e09cf28a3eafdfa3e888aacc
grabner <pjg.github@ubergrabner.net>
2013-07-07 14:22:32 -0400

    added related content fetching

--- templatealchemy/pkg.py
+++ templatealchemy/pkg.py
@@ -7,7 +7,8 @@
 # copy: (C) Copyright 2013 Cadit Health Inc., All Rights Reserved.
 #------------------------------------------------------------------------------
 
-import pkgutil
+import pkgutil, pkg_resources, os.path
+from StringIO import StringIO
 from templatealchemy import api, util
 
 #------------------------------------------------------------------------------
@@ -26,8 +27,28 @@ class PkgSource(api.Source):
     return PkgSource(self.module + ':' + self.path + '/' + name)
 
   #----------------------------------------------------------------------------
+  def getFormats(self):
+    # todo: is there any way to replicate this pkg_resources functionality
+    #       with pkgutil or some other standard library?...
+    path = os.path.dirname(self.path)
+    base = os.path.basename(self.path) + '.'
+    ret = []
+    for cur in pkg_resources.resource_listdir(self.module, path):
+      if cur.startswith(base):
+        ret.append(cur[len(base):])
+    return ret
+
+  #----------------------------------------------------------------------------
   def get(self, format):
-    return pkgutil.get_data(self.module, self.path + '.' + format)
+    return StringIO(pkgutil.get_data(self.module, self.path + '.' + format))
+
+  #----------------------------------------------------------------------------
+  def getRelated(self, name):
+    if name.startswith('/'):
+      name = name[1:]
+    else:
+      name = os.path.dirname(self.path) + '/' + name
+    return StringIO(pkgutil.get_data(self.module, name))
 
 #------------------------------------------------------------------------------
 # end of $Id$
