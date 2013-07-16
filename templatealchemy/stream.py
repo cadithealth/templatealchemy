43093426f2eb7cba1439734fbed1bcad3ba97b2b
grabner <pjg.github@ubergrabner.net>
2013-07-16 14:02:58 -0400

    moved to buffered by default for "stream" type sources

--- templatealchemy/stream.py
+++ templatealchemy/stream.py
@@ -9,10 +9,15 @@
 
 '''
 A TemplateAlchemy source that gets the template source from a
-file-like object. Note that the file is not buffered, and therefore
-the template is "single-use".
+file-like object. Note that the file is by default buffered into
+memory (so that it can be served multiple times) -- see the
+`
 '''
 
+# todo: really, any system that expects to be able to call a template
+#       multiple times should do the buffering itself...
+
+from StringIO import StringIO
 from templatealchemy import api, util
 
 #------------------------------------------------------------------------------
@@ -23,8 +28,9 @@ def loadSource(spec=None):
 class StreamSource(api.Source):
 
   #----------------------------------------------------------------------------
-  def __init__(self, spec):
-    self.stream = spec
+  def __init__(self, spec, replayable=True):
+    self.stream  = spec
+    self._buffer = replayable
 
   #----------------------------------------------------------------------------
   def getSource(self, name):
@@ -36,7 +42,11 @@ class StreamSource(api.Source):
 
   #----------------------------------------------------------------------------
   def get(self, format):
-    return self.stream
+    if self._buffer is False:
+      return self.stream
+    if self._buffer is None or self._buffer is True:
+      self._buffer = self.stream.read()
+    return StringIO(self._buffer)
 
   #----------------------------------------------------------------------------
   def getRelated(self, name):
