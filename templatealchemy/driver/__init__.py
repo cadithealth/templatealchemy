5cea973de0977a5e04da12406466e011b8ddd3f0
metagriffin <mg.github@uberdev.org>
2013-07-29 13:27:19 -0400

    moved all drivers to separate "templatealchemy.driver" namespace module

---
+++ templatealchemy/driver/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#------------------------------------------------------------------------------
+# file: $Id$
+# auth: Philip J Grabner <grabner@cadit.com>
+# date: 2013/07/29
+# copy: (C) Copyright 2013 Cadit Health Inc., All Rights Reserved.
+#------------------------------------------------------------------------------
+
+try:
+  __import__('pkg_resources').declare_namespace(__name__)
+except ImportError:
+  __path__ = __import__('pkgutil').extend_path(__path__, __name__)
+
+#------------------------------------------------------------------------------
+# end of $Id$
+#------------------------------------------------------------------------------
