03eb7e6588c64ea62929ae88bb49de7bbd30f5a6
grabner <pjg.github@ubergrabner.net>
2013-07-29 13:41:32 -0400

    added top-level namespace module work-around

--- templatealchemy/__init__.py
+++ templatealchemy/__init__.py
@@ -1,14 +1,28 @@
 # -*- coding: utf-8 -*-
 #------------------------------------------------------------------------------
 # file: $Id$
-# lib:  templatealchemy
 # auth: Philip J Grabner <grabner@cadit.com>
-# date: 2013/07/03
+# date: 2013/07/29
 # copy: (C) Copyright 2013 Cadit Health Inc., All Rights Reserved.
 #------------------------------------------------------------------------------
 
-from templatealchemy.engine import *
-from templatealchemy.api import *
+try:
+  __import__('pkg_resources').declare_namespace(__name__)
+except ImportError:
+  __path__ = __import__('pkgutil').extend_path(__path__, __name__)
+
+# todo: ugh. this is ugly, but there is just no other way to make
+#       templatealchemy.driver a namespace package *AND* provide
+#       useful functionality in the `templatealchemy` module...
+#       basically, every TA driver provider *MUST* have a copy of this
+#       file as the templatealchemy/__init__.py file.
+#
+#       yuck.
+
+try:
+  from templatealchemy.common import *
+except ImportError:
+  pass
 
 #------------------------------------------------------------------------------
 # end of $Id$
---
+++ templatealchemy/common.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+#------------------------------------------------------------------------------
+# file: $Id$
+# lib:  templatealchemy.common
+# auth: Philip J Grabner <grabner@cadit.com>
+# date: 2013/07/03
+# copy: (C) Copyright 2013 Cadit Health Inc., All Rights Reserved.
+#------------------------------------------------------------------------------
+
+from templatealchemy.engine import *
+from templatealchemy.api import *
+
+#------------------------------------------------------------------------------
+# end of $Id$
+#------------------------------------------------------------------------------
