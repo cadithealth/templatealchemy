54293226b3d1a6bc24be96b122e1c39ad2fbc2a6
grabner <pjg.github@ubergrabner.net>
2013-08-12 15:11:53 -0400

    bumped version

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ entrypoints = {
 
 setup(
   name                  = 'TemplateAlchemy',
-  version               = '0.1.17',
+  version               = '0.1.18',
   description           = 'An un-opinionated template abstraction layer',
   long_description      = README,
   classifiers           = [
